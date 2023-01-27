49abec1bc8dcac5bcf8a4ad7a9e6ac5457310f8c
Vardhan Patil(DitherWither) <demonslayervardhan@gmail.com>
2023-01-27 11:39:00 +0530

    Fixed data not being loaded on prod builds

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,5 @@
 import { Post } from '$lib/post';
-import { collection, getDocs, limit, orderBy, query, QuerySnapshot } from 'firebase/firestore';
-import type { DocumentData } from 'firebase/firestore';
+import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
 import { firestore } from '$lib/firebase';
 
 export async function load(): Promise<{ posts: { post: Post; post_id: string }[] }> {
@@ -8,12 +7,12 @@ export async function load(): Promise<{ posts: { post: Post; post_id: string }[]
 	const postsCollection = collection(firestore, 'posts');
 	const q = query(postsCollection, orderBy('timestamp', 'desc'), limit(128));
 
-	getDocs(q).then((querySnap: QuerySnapshot<DocumentData>) => {
-		querySnap.forEach((doc) => {
-			const docData = doc.data();
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			posts.push({ post: new Post(docData as any), post_id: doc.id });
-		});
+	const querySnap = await getDocs(q);
+
+	querySnap.forEach((doc) => {
+		const docData = doc.data();
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		posts.push({ post: new Post(docData as any), post_id: doc.id });
 	});
 
 	return { posts };
